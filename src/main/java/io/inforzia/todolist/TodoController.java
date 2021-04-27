31ca4a46a192e58bc0c79cd54362876c03618bde
sehee <v0bu0bu@gmail.com>
2021-04-27 17:41:34 +0900

    delCompletedTodo for문에서 stream으로 수정

--- src/main/java/io/inforzia/todolist/TodoController.java
+++ src/main/java/io/inforzia/todolist/TodoController.java
@@ -4,7 +4,9 @@ package io.inforzia.todolist;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +21,7 @@ public class TodoController {
     @Autowired//더 찾아보기
     private TodoItemRepository todoItemRepository;
 
-    @PostMapping("/todo")
+    @PostMapping(value = "/todo", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveTodo(@RequestBody TodoItem todoItem){
         TodoItem savedTodo = todoItemRepository.save(todoItem);
         return ResponseEntity.ok(savedTodo);
@@ -62,10 +64,8 @@ public class TodoController {
     }
     @DeleteMapping("/todo/completed")
     public void delCompletedTodo(){
-        for (TodoItem delTodo:
-                todoItemRepository.findByCompleted(true)) {
-            deleteTodo(delTodo.getId());
-        }
+        val test = new String();
+        todoItemRepository.findByCompleted(true).stream().forEach(item -> deleteTodo(item.getId()));
     }
     @GetMapping("/todo/{id}")
     public TodoItem getTodo(@PathVariable Integer id){
