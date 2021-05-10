a0c88c8b0144548c45792a573725cfd8b04db13a
sehee <v0bu0bu@gmail.com>
2021-05-10 09:11:04 +0900

    header 추가

--- src/main/java/io/inforzia/todolist/TodolistApplication.java
+++ src/main/java/io/inforzia/todolist/TodolistApplication.java
@@ -2,12 +2,9 @@ package io.inforzia.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class TodolistApplication {
 	public static void main(String[] args) {
